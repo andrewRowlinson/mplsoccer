c08230baadb627947646ca0d95222352b5d85698
Andrew Rowlinson <andrew.rowlinson@bluugo.fi>
2020-03-25 23:21:13 +0200

    Adding info to setup file.

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup
 
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
 setup(name='mplsoccer',
       version='0.0.0',
       description='mplsoccer is a Python plotting library for drawing soccer / football pitches quickly in Matplotlib.',
+      long_description_content_type = "text/markdown",
+      long_description = README,
       url='https://github.com/andrewRowlinson/mplsoccer',
       author='Andrew Rowlinson',
       author_email='rowlinsonandy@gmail.com',
+      author_twitter='@numberstorm',
       license='MIT',
       packages=['mplsoccer'],
+      python_requires='>=3.6',
       zip_safe=False)
+
+install_requires=['matplotlib',
+                  'seaborn',
+                  'numpy']
