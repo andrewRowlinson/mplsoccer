14f4fa9ab318ce29084dd3030657cdcb80d1c42b
Andrew Rowlinson <andrew.rowlinson@bluugo.fi>
2020-04-19 19:41:32 +0300

    Minor pep8 fixes.

--- docs/source/gallery/plot_arrows.py
+++ docs/source/gallery/plot_arrows.py
@@ -11,10 +11,11 @@ from mplsoccer.statsbomb import read_event, EVENT_SLUG
 from matplotlib import rcParams
 import os
 
-rcParams['text.color'] = '#c7d5cc' # set the default text color
+rcParams['text.color'] = '#c7d5cc'  # set the default text color
 
 # get event dataframe for game 7478, create a dataframe of the passes, and a boolean mask for the outcome
-df = read_event(os.path.join(EVENT_SLUG,'7478.json'), related_event_df = False, shot_freeze_frame_df = False, tactics_lineup_df = False)['event']
+df = read_event(os.path.join(EVENT_SLUG, '7478.json'),
+                related_event_df=False, shot_freeze_frame_df=False, tactics_lineup_df=False)['event']
 
 ##############################################################################
 # Boolean mask for filtering the dataset by team
@@ -25,7 +26,7 @@ mask_team1 = (df.type_name == 'Pass') & (df.team_name == team1)
 ##############################################################################
 # Filter dataset to only include one teams passes and get boolean mask for the completed passes
 
-df_pass = df.loc[mask_team1, ['x','y','pass_end_x','pass_end_y','outcome_name']]
+df_pass = df.loc[mask_team1, ['x', 'y', 'pass_end_x', 'pass_end_y', 'outcome_name']]
 mask_complete = df_pass.outcome_name.isnull()
 
 ##############################################################################
@@ -37,29 +38,28 @@ df_pass.head()
 # Plotting
 
 # Setup the pitch
-pitch = Pitch(pitch_type = 'statsbomb', orientation = 'horizontal',
-              pitch_color = '#22312b', line_color = '#c7d5cc', figsize = (16, 9))
+pitch = Pitch(pitch_type='statsbomb', orientation='horizontal',
+              pitch_color='#22312b', line_color='#c7d5cc', figsize=(16, 9))
 fig, ax = pitch.draw()
 
 # Plot the completed passes
 pitch.quiver(df_pass[mask_complete].x, df_pass[mask_complete].y,
-             df_pass[mask_complete].pass_end_x, df_pass[mask_complete].pass_end_y, width = 1,
-             headwidth = 10, headlength = 10, color = '#ad993c', ax = ax, label = 'completed passes')
+             df_pass[mask_complete].pass_end_x, df_pass[mask_complete].pass_end_y, width=1,
+             headwidth=10, headlength=10, color='#ad993c', ax=ax, label='completed passes')
 
 # Plot the other passes
 pitch.quiver(df_pass[~mask_complete].x, df_pass[~mask_complete].y,
-             df_pass[~mask_complete].pass_end_x, df_pass[~mask_complete].pass_end_y, width = 1, 
-             headwidth = 10, headlength = 10, color = '#ba4f45', ax = ax, label = 'other passes')
+             df_pass[~mask_complete].pass_end_x, df_pass[~mask_complete].pass_end_y, width=1,
+             headwidth=10, headlength=10, color='#ba4f45', ax=ax, label='other passes')
 
 # setup the legend
-ax.legend(facecolor = '#22312b', edgecolor = 'None', fontsize = 'large')
+ax.legend(facecolor='#22312b', edgecolor='None', fontsize='large')
 
 # Set the title
-ax.set_title(f'{team1} passes vs {team2}', fontsize = 30);
+ax.set_title(f'{team1} passes vs {team2}', fontsize=30)
 
 # Set the figure facecolor
 fig.set_facecolor('#22312b')
 
 # Turn off constrained layout
 fig.set_constrained_layout(False)
-
