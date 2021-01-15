c90b8f0e87382429c46700926dea12e8fdfdc67a
andrew.rowlinson <andrew.rowlinson@bluugo.fi>
2021-01-15 20:36:01 +0200

    amended to reflect dimensions stored in self.dim. If the pitch is inverted flip the rectangle width so the rectangle goes the right way

--- mplsoccer/pitch.py
+++ mplsoccer/pitch.py
@@ -10,49 +10,56 @@ __all__ = ['Pitch', 'VerticalPitch']
 
 
 class Pitch(BasePitch):
+    
+    def _scale_pad(self):
+        self.pad_left = self.pad_left * self.dim.aspect
+        self.pad_right = self.pad_right * self.dim.aspect      
         
     def _set_extent(self):
-        extent = np.array([self.left, self.right, self.bottom, self.top], dtype=np.float32)
+        extent = np.array([self.dim.left, self.dim.right, self.dim.bottom, self.dim.top], dtype=np.float32)
         pad = np.array([-self.pad_left, self.pad_right, -self.pad_bottom, self.pad_top], dtype=np.float32)
         visible_pad = np.clip(np.array([self.pad_left, self.pad_right,
                                         self.pad_bottom, self.pad_top], dtype=np.float32),
                               a_min=None, a_max=0.)
         visible_pad[[0, 2]] = - visible_pad[[0, 2]]
         if self.half:
-            extent[0] = self.center_length  # pitch starts at center line
+            extent[0] = self.dim.center_length  # pitch starts at center line
             visible_pad[0] = - self.pad_left  # do not want clipped values if half
-        if self.invert_y:  # when inverted the padding is negative
+        if self.dim.invert_y:  # when inverted the padding is negative
             pad[2:] = -pad[2:]
             visible_pad[2:] = - visible_pad[2:]
         self.extent = extent + pad
+        self.ax_aspect = abs(self.extent[1] - self.extent[0]) / (abs(self.extent[3] - self.extent[2]) * self.dim.aspect)
         self.visible_pitch = extent + visible_pad
         if self.half:
-            extent[0] = extent[0] - min(self.pad_left, self.pitch_length/2)
+            extent[0] = extent[0] - min(self.pad_left, self.dim.pitch_length/2)
             
         # hexbin
         self.hexbin_gridsize = (17, 8)
-        self.hex_extent = np.array([min(self.left, self.right), max(self.left, self.right),
-                                    min(self.bottom, self.top), max(self.bottom, self.top)], dtype=np.float32)
+        self.hex_extent = np.array([self.dim.left, self.dim.right, min(self.dim.bottom, self.dim.top),
+                                    max(self.dim.bottom, self.dim.top)], dtype=np.float32)
         
         # kdeplot
-        self.kde_clip = ((self.left, self.right), (self.bottom, self.top))
+        self.kde_clip = ((self.dim.left, self.dim.right), (self.dim.bottom, self.dim.top))
         
         # lines
-        self.reverse_cmap = self.invert_y
+        self.reverse_cmap = self.dim.invert_y
         
         # stripe
         total_height = abs(self.extent[3] - self.extent[2])
         pad_top, pad_bottom = -min(self.pad_top, 0), min(self.pad_bottom, 0)
-        if self.invert_y:
+        if self.dim.invert_y:
             pad_top, pad_bottom = -pad_top, -pad_bottom
-        top_side = abs(self.extent[2] - self.top + pad_top)
-        bottom_side = abs(self.extent[2] - self.bottom + pad_bottom)
+        top_side = abs(self.extent[2] - self.dim.top + pad_top)
+        bottom_side = abs(self.extent[2] - self.dim.bottom + pad_bottom)
         self.stripe_end = top_side / total_height
         self.stripe_start = bottom_side / total_height
         self.grass_stripe_end = int((1 - self.stripe_start) * 1000)
-        self.grass_stripe_start = int((1 - self.stripe_end) * 1000)
+        self.grass_stripe_start = int((1 - self.stripe_end) * 1000)     
 
     def _draw_rectangle(self, ax, x, y, width, height, **kwargs):
+        if self.dim.invert_y:
+            height = - height
         rectangle = patches.Rectangle((x, y), width, height, **kwargs)
         ax.add_patch(rectangle)
         return rectangle
@@ -70,18 +77,20 @@ class Pitch(BasePitch):
         ax.add_patch(arc)
                 
     def _draw_stripe(self, ax, i):
-        ax.axvspan(self.stripe_locations[i], self.stripe_locations[i + 1],  # note axvspan
+        ax.axvspan(self.dim.stripe_locations[i], self.dim.stripe_locations[i + 1],  # note axvspan
                    self.stripe_start, self.stripe_end,
                    facecolor=self.stripe_color, zorder=self.stripe_zorder)
         
     def _draw_stripe_grass(self, pitch_color):
         total_width = self.extent[1] - self.extent[0]
-        for i in range(len(self.stripe_locations) - 1):
+        for i in range(len(self.dim.stripe_locations) - 1):
             if i % 2 == 0:
-                if ((self.extent[0] <= self.stripe_locations[i] <= self.extent[1]) or
-                        (self.extent[0] <= self.stripe_locations[i + 1] <= self.extent[1])):
-                    start = int((max(self.stripe_locations[i], self.extent[0]) - self.extent[0]) / total_width * 1000)
-                    end = int((min(self.stripe_locations[i+1], self.extent[1]) - self.extent[0]) / total_width * 1000)    
+                if ((self.extent[0] <= self.dim.stripe_locations[i] <= self.extent[1]) or
+                        (self.extent[0] <= self.dim.stripe_locations[i + 1] <= self.extent[1])):
+                    start = (int((max(self.dim.stripe_locations[i], self.extent[0]) -
+                                  self.extent[0]) / total_width * 1000))
+                    end = (int((min(self.dim.stripe_locations[i+1], self.extent[1]) -
+                                self.extent[0]) / total_width * 1000))
                     pitch_color[self.grass_stripe_start: self.grass_stripe_end, start: end] = \
                         pitch_color[self.grass_stripe_start: self.grass_stripe_end, start: end] + 2
         return pitch_color
@@ -124,50 +133,58 @@ class Pitch(BasePitch):
 
 class VerticalPitch(BasePitch):
     
+    def _scale_pad(self):
+        self.pad_bottom = self.pad_bottom * self.dim.aspect
+        self.pad_top = self.pad_top * self.dim.aspect 
+   
     def _set_extent(self):
-        extent = np.array([self.top, self.bottom, self.left, self.right], dtype=np.float32)
-        pad = np.array([self.pad_left, -self.pad_right, -self.pad_bottom, self.pad_top], dtype=np.float32)
+        extent = np.array([self.dim.top, self.dim.bottom, self.dim.left, self.dim.right], dtype=np.float32)
+        pad = np.array([self.pad_left, -self.pad_right, -self.pad_bottom,
+                        self.pad_top], dtype=np.float32)
         visible_pad = np.clip(np.array([self.pad_left, self.pad_right,
                                         self.pad_bottom, self.pad_top], dtype=np.float32),
                               a_min=None, a_max=0.)
         visible_pad[[1, 2]] = - visible_pad[[1, 2]]
         if self.half:
-            extent[2] = self.center_length  # pitch starts at center line
+            extent[2] = self.dim.center_length  # pitch starts at center line
             visible_pad[2] = - self.pad_bottom  # do not want clipped values if half
-        if self.invert_y:  # when inverted the padding is negative
+        if self.dim.invert_y:  # when inverted the padding is negative
             pad[0:2] = -pad[0:2]
             visible_pad[0:2] = - visible_pad[0:2]
         self.extent = extent + pad
+        self.ax_aspect = abs(self.extent[1] - self.extent[0]) / (abs(self.extent[3] - self.extent[2]) * self.dim.aspect)
         self.visible_pitch = extent + visible_pad
         if self.half:
-            extent[2] = extent[2] - min(self.pad_bottom, self.pitch_length/2)
-        self.aspect = 1 / self.aspect
+            extent[2] = extent[2] - min(self.pad_bottom, self.dim.pitch_length/2)
+        self.dim.aspect = 1 / self.dim.aspect
         
         # hexbin
         self.hexbin_gridsize = (17, 17)
-        self.hex_extent = np.array([min(self.bottom, self.top), max(self.bottom, self.top),
-                                    min(self.left, self.right), max(self.left, self.right)], dtype=np.float32)
+        self.hex_extent = np.array([min(self.dim.bottom, self.dim.top), max(self.dim.bottom, self.dim.top),
+                                    self.dim.left, self.dim.right], dtype=np.float32)
         
         # kdeplot
-        self.kde_clip = ((self.top, self.bottom), (self.left, self.right))
-        
+        self.kde_clip = ((self.dim.top, self.dim.bottom), (self.dim.left, self.dim.right))
+
         # lines
         self.reverse_cmap = False
         
         # stripe
         total_height = abs(self.extent[1] - self.extent[0])
         pad_top, pad_bottom = -min(self.pad_left, 0), min(self.pad_right, 0)
-        if self.invert_y:
+        if self.dim.invert_y:
             pad_top, pad_bottom = -pad_top, -pad_bottom
-        top_side = abs(self.extent[0] - self.top + pad_top)
-        bottom_side = abs(self.extent[0] - self.bottom + pad_bottom)
+        top_side = abs(self.extent[0] - self.dim.top + pad_top)
+        bottom_side = abs(self.extent[0] - self.dim.bottom + pad_bottom)
         self.stripe_start = top_side / total_height
         self.stripe_end = bottom_side / total_height
         self.grass_stripe_end = int(self.stripe_end * 1000)
         self.grass_stripe_start = int(self.stripe_start * 1000)
        
     def _draw_rectangle(self, ax, x, y, width, height, **kwargs):
-        rectangle = patches.Rectangle((y, x), height, width, **kwargs)
+        if self.dim.invert_y:
+            height = - height
+        rectangle = patches.Rectangle((y, x), height, width, **kwargs)       
         ax.add_patch(rectangle)
         return rectangle
         
@@ -184,19 +201,19 @@ class VerticalPitch(BasePitch):
         ax.add_patch(arc)
         
     def _draw_stripe(self, ax, i):
-        ax.axhspan(self.stripe_locations[i], self.stripe_locations[i + 1],  # note axhspan
+        ax.axhspan(self.dim.stripe_locations[i], self.dim.stripe_locations[i + 1],  # note axhspan
                    self.stripe_start, self.stripe_end,
                    facecolor=self.stripe_color, zorder=self.stripe_zorder)
 
     def _draw_stripe_grass(self, pitch_color):
         total_width = self.extent[3] - self.extent[2] 
-        for i in range(len(self.stripe_locations) - 1):
+        for i in range(len(self.dim.stripe_locations) - 1):
             if i % 2 == 0:
-                if ((self.extent[2] <= self.stripe_locations[i] <= self.extent[3]) or
-                        (self.extent[2] <= self.stripe_locations[i + 1] <= self.extent[3])):
-                    start = (1000 - int((min(self.stripe_locations[i+1], self.extent[3]) - self.extent[2])
+                if ((self.extent[2] <= self.dim.stripe_locations[i] <= self.extent[3]) or
+                        (self.extent[2] <= self.dim.stripe_locations[i + 1] <= self.extent[3])):
+                    start = (1000 - int((min(self.dim.stripe_locations[i+1], self.extent[3]) - self.extent[2])
                                         / total_width * 1000))
-                    end = (1000 - int((max(self.stripe_locations[i], self.extent[2]) - self.extent[2])
+                    end = (1000 - int((max(self.dim.stripe_locations[i], self.extent[2]) - self.extent[2])
                                       / total_width * 1000))
                     pitch_color[start: end, self.grass_stripe_start: self.grass_stripe_end] = \
                         pitch_color[start: end, self.grass_stripe_start: self.grass_stripe_end] + 2
