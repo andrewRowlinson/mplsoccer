7ce8925f33b1793ed23bdb1ae1d19966f4235d12
andrew.rowlinson <andrew.rowlinson@bluugo.fi>
2021-04-05 21:58:29 +0300

    improved hexbin example

--- examples/pitch_plots/plot_hexbin.py
+++ examples/pitch_plots/plot_hexbin.py
@@ -17,7 +17,7 @@ from mplsoccer import VerticalPitch, add_image, FontManager
 from mplsoccer.statsbomb import read_event, EVENT_SLUG
 
 ##############################################################################
-# load first game that Messi played as a false-9 and the match before
+# Load the first game that Messi played as a false-9 and the match before.
 kwargs = {'related_event_df': False, 'shot_freeze_frame_df': False,
           'tactics_lineup_df': False, 'warn': False}
 df_false9 = read_event(f'{EVENT_SLUG}/69249.json', **kwargs)['event']
@@ -27,24 +27,27 @@ df_false9 = df_false9.loc[df_false9.player_id == 5503, ['x', 'y']]
 df_before_false9 = df_before_false9.loc[df_before_false9.player_id == 5503, ['x', 'y']]
 
 ##############################################################################
-# Create a custom colormap
-# Note see the Custom colormaps example for more ideas
+# Create a custom colormap.
+# Note see the`custom colormaps
+# <https://mplsoccer.readthedocs.io/en/latest/gallery/pitch_plots/plot_cmap.html>`_
+# example for more ideas.
 flamingo_cmap = LinearSegmentedColormap.from_list("Flamingo - 10 colors",
                                                   ['#e3aca7', '#c03a1d'], N=10)
 
 ##############################################################################
-# Plot Messi's game before becoming a false-9
-pitch = VerticalPitch(line_color='#000009', line_zorder=2, pitch_color='white')
-fig, ax = pitch.draw(figsize=(4.4, 6.4))
-hexmap = pitch.hexbin(df_before_false9.x, df_before_false9.y, ax=ax, edgecolors='#f4f4f4',
-                      gridsize=(8, 8), cmap=flamingo_cmap)
-
-##############################################################################
-# Plot Messi's first game as a false-9
+# Plot Messi's first game as a false-9.
 pitch = VerticalPitch(line_color='#000009', line_zorder=2, pitch_color='white')
 fig, ax = pitch.draw(figsize=(4.4, 6.4))
 hexmap = pitch.hexbin(df_false9.x, df_false9.y, ax=ax, edgecolors='#f4f4f4',
                       gridsize=(8, 8), cmap=flamingo_cmap)
+
+##############################################################################
+# Load a custom font.
+URL = 'https://github.com/googlefonts/roboto/blob/main/src/hinted/Roboto-Regular.ttf?raw=true'
+robotto_regular = FontManager(URL)
+
+##############################################################################
+# Load images.
 
 # Load the StatsBomb logo and Messi picture
 MESSI_URL = 'https://upload.wikimedia.org/wikipedia/commons/b/b8/Messi_vs_Nigeria_2018.jpg'
@@ -53,7 +56,30 @@ SB_LOGO_URL = 'https://raw.githubusercontent.com/statsbomb/open-data/master/img/
 sb_logo = Image.open(urlopen(SB_LOGO_URL))
 
 ##############################################################################
-# Plot Messi's game before and after becoming a false-9
+# Plot the chart again with a title.
+# We will use mplsoccer's grid function to plot a pitch with a title and endnote axes.
+
+fig, pitch_ax, title_ax, endnote_ax = pitch.grid(figheight=10, title_height=0.08, endnote_space=0,
+                                                 title_space=0,
+                                                 grid_height=0.82, endnote_height=0.05)
+hexmap = pitch.hexbin(df_false9.x, df_false9.y, ax=pitch_ax, edgecolors='#f4f4f4',
+                      gridsize=(8, 8), cmap=flamingo_cmap)
+endnote_ax.text(1, 0.5, '@your_twitter_handle', va='center', ha='right', fontsize=15,
+                fontproperties=robotto_regular.prop)
+endnote_ax.axis('off')
+title_ax.text(0.5, 0.7, "Lionel Messi's Actions", color='#000009',
+              va='center', ha='center', fontproperties=robotto_regular.prop, fontsize=30)
+title_ax.text(0.5, 0.25, "First game as a false nine", color='#000009',
+              va='center', ha='center', fontproperties=robotto_regular.prop, fontsize=20)
+title_ax.axis('off')
+ax_sb_logo = add_image(sb_logo, fig,
+                       # set the left, bottom and height to align with the endnote
+                       left=endnote_ax.get_position().x0,
+                       bottom=endnote_ax.get_position().y0,
+                       height=endnote_ax.get_position().height)
+
+##############################################################################
+# Plot Messi's actions in the matches before and after becoming a false-9.
 # We will use mplsoccer's grid function, which is a convenient way to plot a grid
 # of pitches with a title and endnote axes.
 
@@ -75,8 +101,7 @@ ax_messi = add_image(messi_image, fig, interpolation='hanning',
                      height=title_ax.get_position().height)
 
 # titles using highlight_text and a google font (Robotto)
-URL = 'https://github.com/googlefonts/roboto/blob/main/src/hinted/Roboto-Regular.ttf?raw=true'
-robotto_regular = FontManager(URL)
+
 TITLE_STR1 = 'The Evolution of Lionel Messi'
 TITLE_STR2 = 'Actions in the match <before> and\n<after> becoming a False-9'
 title1_text = title_ax.text(0.5, 0.7, TITLE_STR1, fontsize=28, color='#000009',
@@ -89,6 +114,8 @@ ax_text(0.5, 0.3, TITLE_STR2, ha='center', va='center', fontsize=18, color='#000
 # turn off title and endnote axes
 endnote_ax.axis('off')
 title_ax.axis('off')
+
+# sphinx_gallery_thumbnail_path = 'gallery/pitch_plots/images/sphx_glr_plot_hexbin_003.png'
 
 # Messi Photo from: https://en.wikipedia.org/wiki/Lionel_Messi#/media/File:Messi_vs_Nigeria_2018.jpg
 # License: https://creativecommons.org/licenses/by-sa/3.0/;
