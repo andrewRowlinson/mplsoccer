d4393921c5025c046d60e321a90e96c3325e1bf7
Andrew Rowlinson <rowlinsonandy@gmail.com>
2022-05-29 21:35:06 +0300

    updated the StatsBomb data parsers to use less pandas and make the API interface better. Bumped the version number as breaking changes. Updated the init.

--- mplsoccer/__init__.py
+++ mplsoccer/__init__.py
@@ -1,15 +1,15 @@
 """ This module imports the mplsoccer classes/ functions so that they can be used like
 from mplsoccer import Pitch."""
 
-from mplsoccer._version import __version__
-from mplsoccer import statsbomb
-from mplsoccer.cm import *
-from mplsoccer.linecollection import *
-from mplsoccer.pitch import *
-from mplsoccer.quiver import *
-from mplsoccer.radar_chart import *
-from mplsoccer.scatterutils import *
-from mplsoccer.utils import *
-from mplsoccer.bumpy_chart import *
-from mplsoccer.py_pizza import *
-from mplsoccer.grid import *
+from ._version import __version__
+from .statsbomb import Sbopen, Sbapi
+from .cm import *
+from .linecollection import *
+from .pitch import *
+from .quiver import *
+from .radar_chart import *
+from .scatterutils import *
+from .utils import *
+from .bumpy_chart import *
+from .py_pizza import *
+from .grid import *
