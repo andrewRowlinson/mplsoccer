b2c67b5b79045f1521f1be14d1814e10859d5a0c
Florian Schmitt <florian.schmitt94@gmail.com>
2023-03-16 17:04:02 +0100

    add pitch_type 'impect'

--- mplsoccer/dimensions.py
+++ mplsoccer/dimensions.py
@@ -59,7 +59,8 @@ from typing import Optional
 import numpy as np
 
 valid = ['statsbomb', 'tracab', 'opta', 'wyscout', 'uefa',
-         'metricasports', 'custom', 'skillcorner', 'secondspectrum']
+         'metricasports', 'custom', 'skillcorner', 'secondspectrum',
+         'impect']
 size_varies = ['tracab', 'metricasports', 'custom', 'skillcorner', 'secondspectrum']
 
 
@@ -327,6 +328,16 @@ def tracab_dims(pitch_width, pitch_length):
                               center_width=0., center_length=0., circle_diameter=1830.,
                               corner_diameter=200., arc=53.05, invert_y=False, origin_center=True)
 
+def impect_dims():
+    """ Create 'impect' dimensions."""
+    return VariableCenterDims(aspect=1., pitch_width=68, pitch_length=105,
+                              goal_width=7.32, goal_length=2., goal_bottom=-3.66, goal_top=3.66,
+                              six_yard_width=18.32, six_yard_length=5.5, six_yard_bottom=-9.16,
+                              six_yard_top=9.16, penalty_spot_distance=11.,
+                              penalty_area_width=40.32, penalty_area_length=16.5,
+                              penalty_area_bottom=-20.16, penalty_area_top=20.16, center_width=0.,
+                              center_length=0., circle_diameter=18.3, corner_diameter=2., arc=53.05,
+                              invert_y=False, origin_center=True)
 
 def custom_dims(pitch_width, pitch_length):
     """ Create 'custom' dimensions."""
@@ -375,4 +386,6 @@ def create_pitch_dims(pitch_type, pitch_width=None, pitch_length=None):
         pitch_width = pitch_width * 100.
         pitch_length = pitch_length * 100.
         return tracab_dims(pitch_width, pitch_length)
+    if pitch_type == 'impect':
+        return impect_dims()
     return custom_dims(pitch_width, pitch_length)
