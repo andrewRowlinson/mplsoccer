2646f59eb80597a31e818abb5fc2b7794cef5f1d
Andrew Rowlinson <andrew.rowlinson@finnair.com>
2024-07-31 22:55:47 +0300

    typos

--- examples/pitch_setup/plot_pitch_types.py
+++ examples/pitch_setup/plot_pitch_types.py
@@ -20,7 +20,7 @@ print(valid)
 # The default pitch is `StatsBomb <https://statsbomb.com/>`_
 # The xaxis limits are 0 to 120 and the yaxis limits are 80 to 0 (inverted).
 pitch = Pitch(pitch_type='statsbomb', axis=True, label=True)
-fig, axs = pitch.draw()
+fig, ax = pitch.draw()
 
 ##############################################################################
 # Tracab
@@ -30,7 +30,7 @@ fig, axs = pitch.draw()
 # The yaxis limits are -pitch_width/2 * 100 to pitch_width/2 * 100.
 pitch = Pitch(pitch_type='tracab', pitch_width=68, pitch_length=105,
               axis=True, label=True)
-fig, axs = pitch.draw()
+fig, ax = pitch.draw()
 
 ##############################################################################
 # Opta
@@ -38,7 +38,7 @@ fig, axs = pitch.draw()
 # `Opta data from Stats Perform <https://www.statsperform.com/opta/>`_ has
 # both the x and y limits between 0 and 100.
 pitch = Pitch(pitch_type='opta', axis=True, label=True)
-fig, axs = pitch.draw()
+fig, ax = pitch.draw()
 
 ##############################################################################
 # Wyscout
@@ -46,7 +46,7 @@ fig, axs = pitch.draw()
 # `Wyscout data from Hudl <https://footballdata.wyscout.com/>`_ also has
 # both the x and y limits between 0 and 100, but the y-axis is inverted.
 pitch = Pitch(pitch_type='wyscout', axis=True, label=True)
-fig, axs = pitch.draw()
+fig, ax = pitch.draw()
 
 ##############################################################################
 # Custom
@@ -55,7 +55,7 @@ fig, axs = pitch.draw()
 # by changing the pitch_length and pitch_width.
 pitch = Pitch(pitch_type='custom', pitch_width=68, pitch_length=105,
               axis=True, label=True)
-fig, axs = pitch.draw()
+fig, ax = pitch.draw()
 
 ##############################################################################
 # Uefa
@@ -63,7 +63,7 @@ fig, axs = pitch.draw()
 # The uefa pitch is a special case of the custom pitch with the pitch_length
 # and pitch_width set to Uefa's standard (105m * 65m).
 pitch = Pitch(pitch_type='uefa', axis=True, label=True)
-fig, axs = pitch.draw()
+fig, ax = pitch.draw()
 
 ##############################################################################
 # Metricasports
@@ -72,7 +72,7 @@ fig, axs = pitch.draw()
 # pitch limits are between 0 and 1, but the y-axis is inverted.
 pitch = Pitch(pitch_type='metricasports', pitch_length=105, pitch_width=68,
               axis=True, label=True)
-fig, axs = pitch.draw()
+fig, ax = pitch.draw()
 
 ##############################################################################
 # Skillcorner
@@ -82,7 +82,7 @@ fig, axs = pitch.draw()
 # -pitch_length/2 to pitch_length/2.
 pitch = Pitch(pitch_type='skillcorner', pitch_length=105, pitch_width=68,
               axis=True, label=True)
-fig, axs = pitch.draw()
+fig, ax = pitch.draw()
 
 ##############################################################################
 # Second Spectrum
@@ -92,7 +92,7 @@ fig, axs = pitch.draw()
 # -pitch_length/2 to pitch_length/2.
 pitch = Pitch(pitch_type='secondspectrum', pitch_length=105, pitch_width=68,
               axis=True, label=True)
-fig, axs = pitch.draw()
+fig, ax = pitch.draw()
 
 ##############################################################################
 # Impect
@@ -100,7 +100,7 @@ fig, axs = pitch.draw()
 # `Impect <https://www.impect.com/en/>`_
 # has centered pitches from -52.5 to 52.5 (x-axis) and -34 to 34 (y-axis).
 pitch = Pitch(pitch_type='impect', axis=True, label=True)
-fig, axs = pitch.draw()
+fig, ax = pitch.draw()
 
 ##############################################################################
 # Standardized coordinates
@@ -123,9 +123,9 @@ fig, ax = pitch.draw()
 ##############################################################################
 # Other custom dimensions
 # -----------------------
-# Aditionally, you can create your own abitarty dimensions.
-# See the ``mplsoccer.dimensions`` module for examples of how to define the dimensions:
-# https://raw.githubusercontent.com/andrewRowlinson/mplsoccer/main/mplsoccer/dimensions.py.
+# Aditionally, you can create your own arbitrary dimensions.
+# See the `mplsoccer.dimensions module <https://raw.githubusercontent.com/andrewRowlinson/mplsoccer/main/mplsoccer/dimensions.py>`_
+# for examples of how to define the dimensions. 
 # The custom dimensions must be a subclass of ``mplsoccer.dimensions.BaseDims``
 # and can then be passed to the ``pitch_type`` argument.
 
